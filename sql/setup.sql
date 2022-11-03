62eae30b58fe4978122834d0436defd83ff610f6
Tom Booth <tombeezle@gmail.com>
2022-11-03 11:37:12 -0700

    added data table in setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+CREATE TABLE world_champs (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  venue VARCHAR NOT NULL,
+  year INT NOT NULL
+);
+
+INSERT INTO world_champs (name, venue,  year)
+VALUES 
+('Loic Bruni', 'Les Gets', 2022),
+('Greg Minaar', 'Val di Sole', 2021),
+('Reece Wilson', 'Leogang', 2020),
+('Loic Bruni', 'Mont-Sainte-Anne', 2019),
+('Loic Bruni', 'Lenzerheide', 2018),
+('Loic Bruni', 'Cairns', 2017);
